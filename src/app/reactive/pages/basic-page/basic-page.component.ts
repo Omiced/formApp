a29c8a60f60578627c836d7aed6de0605f9bfd08
Josue Cano <jdevelopcano@gmail.com>
2023-08-04 11:14:23 -0600

    terminada una parte

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -25,7 +25,7 @@ export class BasicPageComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
-    this.myForm.reset(rtx5090);
+    // this.myForm.reset(rtx5090);
   }
 
   public myForm: FormGroup = this.fb.group({
@@ -34,8 +34,33 @@ export class BasicPageComponent implements OnInit {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo de ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return 'hol';
+  }
+
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value);
     this.myForm.reset({ price: 0, inStorage: 0 });
   }
